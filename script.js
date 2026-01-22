bbd6acd153a48f7d733815482f64c806d368b7c6
verilog-coyote <88463315+verilog-coyote@users.noreply.github.com>
2026-01-22 11:36:40 -1000

    All content from timeline populates

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 import data from './timeline.json' with {type: 'json'};
 
-console.log(data);
+let timeline = data["timeline"]; // 
+let timeline_element = document.querySelector(".timeline-container");
 
-let timeline = data["timeline"];
 for (let i = 0; i < timeline.length; i++) {
-    console.log(timeline[i]);
+
+    // make a new element for each entry
+    let entry = document.createElement("div");
+    entry.classList.add("timeline-entry");
+
+    // fetch the header from JSON
+    let header = document.createElement("h2");
+    header.textContent = timeline[i].year;
+    header.classList.add("timeline-year");
+
+    // fetch the text content
+    let content = document.createElement("div");
+    content.textContent = timeline[i].content;
+    content.classList.add("timeline-content");
+
+    // add them to the entry
+    entry.append(header);
+    entry.append(content);
+
+    // and add the entry to the timeline itself
+    timeline_element.append(entry);
 }
