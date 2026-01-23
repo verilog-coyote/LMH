032e9ee7b82c3732deb197d020d4694ed748ef5f
verilog-coyote <88463315+verilog-coyote@users.noreply.github.com>
2026-01-23 11:47:40 -1000

    SVG based timeline WIP

--- script.js
+++ script.js
@@ -3,26 +3,84 @@ import data from './timeline.json' with {type: 'json'};
 let timeline = data["timeline"]; // 
 let timeline_element = document.querySelector(".timeline-container");
 
-for (let i = 0; i < timeline.length; i++) {
+function basic_timeline() {
+    // populate timeline HTML with each of the JSON objects
+    for (let i = 0; i < timeline.length; i++) {
 
-    // make a new element for each entry
-    let entry = document.createElement("div");
-    entry.classList.add("timeline-entry");
+        // make a new element for each entry
+        let entry = document.createElement("div");
+        entry.classList.add("timeline-entry");
 
-    // fetch the header from JSON
-    let header = document.createElement("h2");
-    header.textContent = timeline[i].year;
-    header.classList.add("timeline-year");
+        // fetch the header from JSON
+        let header = document.createElement("h2");
+        header.textContent = timeline[i].year;
+        header.classList.add("timeline-year");
 
-    // fetch the text content
-    let content = document.createElement("div");
-    content.textContent = timeline[i].content;
-    content.classList.add("timeline-content");
+        // fetch the text content
+        let content = document.createElement("div");
+        content.textContent = timeline[i].content;
+        content.classList.add("timeline-content");
 
-    // add them to the entry
-    entry.append(header);
-    entry.append(content);
+        // add them to the entry
+        entry.append(header);
+        entry.append(content);
 
-    // and add the entry to the timeline itself
-    timeline_element.append(entry);
+        // and add the entry to the timeline itself
+        timeline_element.append(entry);
+    }
 }
+
+//basic_timeline();
+
+// this is fine and dandy and all but it's not very fancy. and it's not very different.
+// what i want is to create an SVG path, segment it into timeline.length sections, and put circles on each of those points. when the user hovers over each circle, the corresponding entry will appear
+
+function svg_timeline() {
+    // create an SVG element to hold a path
+    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute("width", "300");
+    svg.setAttribute("height", "300");
+
+    // create a path
+    let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute("style", "fill:none;stroke:black;stroke-width:4");
+    path.setAttribute("d", "M 150 5 L75 200 L225 200 Z");
+
+    // add path to the svg element, then add the svg to the html
+    svg.append(path);
+    timeline_element.append(svg);
+
+    // above is our finished path!    
+
+    // calculate distance between markers
+    let svg_length = document.querySelector("path").getTotalLength();
+    let num_segments = 5;
+    let segment_length = svg_length / num_segments;
+    let current_marker = 0;
+
+    for (let i = 0; i <= svg_length; i+= segment_length) {
+        // get the coords of each marker position
+        const point = path.getPointAtLength(i);
+        // and make it a circle
+        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        // set its attributes
+        circle.setAttribute("cx", point.x);
+        circle.setAttribute("cy", point.y);
+        circle.setAttribute("r", 5);
+        circle.setAttribute("fill", "black");
+        circle.setAttribute("style", "cursor:pointer;");
+        circle.setAttribute("value", current_marker)
+        circle.addEventListener("click", function() {handle_marker_click(circle.getAttribute("value"))});
+
+        svg.append(circle);
+        current_marker++; // for determining which point on the timeline we are looking at
+    }
+}
+
+function handle_marker_click(current_marker) {
+    console.log(`Clicked marker: ${timeline[current_marker].year}`);
+}
+
+svg_timeline();
+
